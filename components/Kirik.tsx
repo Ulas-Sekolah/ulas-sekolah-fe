d3e94aae86d5d09894e15c8e2ab233dafc2803c9
Frisko <frisko@koinworks.com>
2021-06-21 17:12:28 +0700

    add unit test

---
+++ components/Kirik.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function Home() {
+  return (
+    <Layout title={process.env.appName}>
+      <h1 className="font-bold text-5xl text-blue-600 ">Hello Kirik</h1>
+    </Layout>
+  )
+}
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,7 +4,9 @@ export default function Layout(props) {
   return(
     <div>
       <Head>
-        <title>{props.title}</title>
+        <title data-testid="titleLayout">{props.title}</title>
+        <span data-testid="text-content">Text Content</span>
+        <div className="foo">bar</div>
         <link rel="icon" href="https://image.shutterstock.com/image-vector/initial-r-rr-icon-logo-600w-1628908060.jpg" />
       </Head>
       {props.children}
