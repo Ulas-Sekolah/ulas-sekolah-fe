d036a7bec30c5d0001270e7d6b2d58bf0482676b
Frisko <frisko@koinworks.com>
2021-07-22 10:44:20 +0700

    add detail page

---
+++ pages/detail.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Detail from '../containers/pages/Detail'
+
+export default function Home() {
+  return (
+    <Layout title={`${process.env.appName} | Review Sekolah`}>
+      <Detail />
+    </Layout>
+  )
+}
