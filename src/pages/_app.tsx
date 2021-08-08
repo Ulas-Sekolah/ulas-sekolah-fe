d614a408650b43ac4864a3d00d046ef06e025a1e
FahrasNH <fahras1998@yahoo.com>
2021-08-08 17:57:27 +0700

    Tailwind setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppWrapper } from 'provider';
 import 'tailwindcss/tailwind.css'
 import 'styles/globals.css'
+import 'styles/tailwind.css';
 import { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
